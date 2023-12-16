03ad7c53a842cdb55413e9b3db0b2891304da577
Emilio González González <emilio@Laptop-de-Emilio.local>
2023-12-16 00:46:29 -0600

    added main

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ dbClinet = DbClient()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+//main.py
+import uvicorn
+
+if __name__ == "__main__":
+  uvicorn.run("server.api:app", host="0.0.0.0", port=8000, reload=True)
